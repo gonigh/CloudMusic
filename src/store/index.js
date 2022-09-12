dcd4d4c76df7ef8d7764032d892f97eb13dc3ab6
gonigh <1364389234@qq.com>
2022-09-12 17:15:40 +0800

    添加pinia状态管理

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import { defineStore } from "pinia";
+
+export const store = defineStore("music-store", {
+  state: () => {
+    return {
+      id: 1,
+    };
+  },
+  getters: {},
+  actions: {},
+});
